2a887c69d0134ba06525823193321d54a846703e
frankyoh <frankyoh@tmon.co.kr>
2019-07-03 20:23:50 +0900

    클래스 이름 변경

--- my-polling-app-server/src/test/java/com/advenoh/mypolls/PollsApplicationTests.java
+++ my-polling-app-server/src/test/java/com/advenoh/mypolls/PollsApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MyPollsApplicationTests {
+public class PollsApplicationTests {
 
 	@Test
 	public void contextLoads() {
