881bfbd6c91419e2749582708d2f71ccd2915824
Frank <frankyoh@tmon.co.kr>
2019-07-04 23:43:05 +0900

    added auditconfiguration file

---
+++ my-polling-app-server/src/main/java/com/advenoh/mypolls/config/AuditingConfig.java
@@ -0,0 +1,9 @@
+package com.advenoh.mypolls.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+}
