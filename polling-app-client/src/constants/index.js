546c13d5ffcc2563579617d849439a26a1f6fc86
kenshin579 <kenshin579@hotmail.com>
2021-03-21 23:24:41 +0900

    [SSR-1] console 추가 for debugging

--- polling-app-client/src/constants/index.js
+++ polling-app-client/src/constants/index.js
@@ -1,4 +1,7 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+console.log("API_BASE_URL", API_BASE_URL)
+console.log("process.env.REACT_APP_API_BASE_URL", process.env.REACT_APP_API_BASE_URL)
+
 export const ACCESS_TOKEN = 'accessToken';
 
 export const POLL_LIST_SIZE = 30;
