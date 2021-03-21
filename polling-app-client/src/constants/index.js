8f5d07584611f408071f90f43053d5e11de93028
kenshin579 <kenshin579@hotmail.com>
2021-03-21 23:33:41 +0900

    [SSR-1] console 수정

--- polling-app-client/src/constants/index.js
+++ polling-app-client/src/constants/index.js
@@ -1,6 +1,5 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
 console.log("API_BASE_URL", API_BASE_URL)
-console.log("process.env.REACT_APP_API_BASE_URL", process.env.REACT_APP_API_BASE_URL)
 
 export const ACCESS_TOKEN = 'accessToken';
 
